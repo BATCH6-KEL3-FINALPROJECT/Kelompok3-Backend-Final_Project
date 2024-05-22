a5e5a20469535e206001e50cd2655c0855e98836
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-05-22 21:22:12 +0700

    Updating : Flight, Airport, Airline Routes on index

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,15 @@ const swaggerUI = require("swagger-ui-express");
 const swaggerDocument = require("../docs/swagger.json");
 
 const Auth = require("./authRouter");
-
+const Flight = require("./flightRouter");
+const Airport = require("./AirportRouter");
+const Airline = require("./airlineRoutes")
 router.use("/api-docs", swaggerUI.serve);
 router.use("/api-docs", swaggerUI.setup(swaggerDocument));
 
 router.use("/api/v1/auth", Auth)
+router.use("/api/v1/flight", Flight)
+router.use("/api/v1/airport", Airport)
+router.use("/api/v1/airline", Airline)
 
 module.exports = router;
