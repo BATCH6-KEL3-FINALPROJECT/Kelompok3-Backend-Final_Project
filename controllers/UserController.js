567910e1ea79cbe1a3cfd8d267b0d13878b2a1ac
Rizki <mhdrizky32@gmail.com>
2024-06-06 21:25:29 +0700

    Adding: User API CRUD

---
+++ controllers/UserController.js
@@ -0,0 +1,144 @@
+const { User } = require("../models");
+const ApiError = require("../utils/apiError");
+const { Op } = require("sequelize");
+
+const findUsers = async (req, res, next) => {
+  try {
+    const { name, email, phone_number, role, is_verified, page, limit } =
+      req.query;
+
+    const pageNum = parseInt(page) || 1;
+    const limitData = parseInt(limit) || 10;
+    const offset = (pageNum - 1) * limitData;
+
+    const whereClause = {};
+    if (name) whereClause.name = { [Op.iLike]: `%${name}%` };
+    if (email) whereClause.email = email;
+    if (phone_number)
+      whereClause.phone_number = { [Op.iLike]: `%${phone_number}%` };
+    if (role) whereClause.role = role;
+    if (is_verified)
+      whereClause.is_verified = { [Op.iLike]: `%${is_verified}%` };
+
+    if (req.query.search) {
+      whereClause[Op.or] = {
+        name: { [Op.like]: `%${req.query.search}%` },
+        email: { [Op.like]: `%${req.query.search}%` },
+        phone_number: { [Op.like]: `%${req.query.search}%` },
+        role: { [Op.like]: `%${req.query.search}%` },
+        is_verified: { [Op.like]: `%${req.query.search}%` },
+      };
+    }
+
+    const { count, rows: users } = await User.findAndCountAll({
+      where: whereClause,
+      offset,
+      limit: limitData,
+      attributes: { exclude: ["password", "refresh_token"] },
+    });
+
+    const totalPages = Math.ceil(count / limitData);
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        users,
+      },
+      pagination: {
+        totalData: count,
+        totalPages,
+        pageNum,
+        limitData,
+      },
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+const findUserById = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["password", "refresh_token"] },
+    });
+
+    if (!user) {
+      return next(
+        new ApiError(`User with ID: ${req.params.id} not found`, 404)
+      );
+    }
+
+    res.status(200).json({
+      status: "Success",
+      data: {
+        user,
+      },
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  const { name, phone_number, email } = req.body;
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return next(new ApiError(`User with ID ${req.params.id} not found`, 404));
+    }
+
+    await User.update(
+      {
+        name,
+        phone_number,
+        email,
+      },
+      {
+        where: {
+          user_id: req.params.id,
+        },
+      }
+    );
+
+    const updatedUser = await User.findByPk(req.params.id);
+
+    res.status(200).json({
+      status: "Success",
+      message: "User updated successful",
+      updatedUser,
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      next(new ApiError(`User with ID ${req.params.id} not found`, 404));
+    }
+
+    await User.destroy({
+      where: {
+        user_id: req.params.id,
+      },
+    });
+
+    res.status(200).json({
+      status: "Success",
+      message: "Successfully deleted user",
+    });
+  } catch (err) {
+    next(new ApiError(err.message, 400));
+  }
+};
+
+module.exports = {
+  findUsers,
+  findUserById,
+  updateUser,
+  deleteUser,
+};
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,15 @@ const swaggerDocument = require("../docs/swagger.json");
 const Auth = require("./authRouter");
 const Flight = require("./flightRouter");
 const Airport = require("./airportRouter");
-const Airline = require("./airlineRoutes")
+const Airline = require("./airlineRoutes");
+const User = require("./userRouter");
 router.use("/api-docs", swaggerUI.serve);
 router.use("/api-docs", swaggerUI.setup(swaggerDocument));
 
-router.use("/api/v1/auth", Auth)
-router.use("/api/v1/flight", Flight)
-router.use("/api/v1/airport", Airport)
-router.use("/api/v1/airline", Airline)
+router.use("/api/v1/auth", Auth);
+router.use("/api/v1/flight", Flight);
+router.use("/api/v1/airport", Airport);
+router.use("/api/v1/airline", Airline);
+router.use("/api/v1/user", User);
 
 module.exports = router;
