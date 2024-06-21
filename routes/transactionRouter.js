8ad9655f69f382e8b081c8443c589b6f2180deea
Azka Zaki <azkazaki11@gmail.com>
2024-06-21 18:58:06 +0700

    Feat/Finishing Payment and transaction

--- routes/transactionRouter.js
+++ routes/transactionRouter.js
@@ -2,7 +2,7 @@ const transaction = require("../controllers/transactionController");
 const authenticate = require("../middlewares/authenticate");
 const router = require('express').Router();
 
-router.post('/create', transaction.createTransactions)
+router.post('/payment/:id', transaction.createTransactions)
 router.post('/booking', authenticate, transaction.createTransactionsWithFlight)
 router.get('/booking/:id', transaction.getBookingData)
 
